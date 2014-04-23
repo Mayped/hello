3fb8b538526ca2d69d3624147a2e795ea5277b5e
Jay Fenwick <fenwickjb@appstate.edu>
2014-04-23 09:29:51 -0400

    fixed this reference

--- Hello.java
+++ Hello.java
@@ -16,7 +16,7 @@ public class Hello {
     }
     public Hello(String name) {
 	greeting = "Hello";
-	name = this.name;//this.name = name;
+	this.name = name;
     }
     public String greet() {
 	return greeting + " " + name;
